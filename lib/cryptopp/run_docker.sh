8c2d7f23a832e69b754360af1258f7b3a4aef73a
kureii (Tomas Adamek) <ad.tomik@seznam.cz>
2023-11-06 19:21:45 +0100

    modern cmake cryptopp replace manual compile library

--- lib/cryptopp/run_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Postav Docker obraz
-docker build -t crypto-compiler .
-
-# Spusť kontejner a proved kompilaci
-# Výsledné knihovny budou uloženy do složky 'output' na hostitelském systému
-docker run --rm -v $(pwd)/output:/output crypto-compiler /workspace/compile.sh
